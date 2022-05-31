0b319e2b388acf5719bcc7e98918c840f34aeea6
Luis Rodriguez <luis.rodriguez@bitekso.com>
2022-05-31 10:28:30 -0400

    fix: se agrego webpack con errrores

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+//@ts-check
+const path = require('path');
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+
+module.exports = {
+	entry: './public/js/app.js',
+	output: {
+		filenanme: 'blundle.js',
+		path: path.join(__dirname, './public/dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+		 				presets: [ '@babel/preset-env' ]
+					}
+				}
+			}
+		]
+	}
+};

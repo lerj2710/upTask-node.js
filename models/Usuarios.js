3e4860c419390891c73b86b7b69a11a4a8a898aa
luis rodriguez <kike.lerj2710@gmail.com>
2022-06-16 18:44:02 -0400

    feat: encriptado contraseña

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -1,22 +1,32 @@
 const Sequelize = require('sequelize');
 const db = require('../config/db');
 const Proyectos = require('../models/Proyectos');
-
-const Usuarios = db.define('usuarios', {
-	id: {
-		type: Sequelize.INTEGER,
-		primaryKey: true,
-		autoIncrement: true
+const bcrypt = require('bcrypt');
+const Usuarios = db.define(
+	'usuarios',
+	{
+		id: {
+			type: Sequelize.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		email: {
+			type: Sequelize.STRING(60),
+			allowNull: false
+		},
+		password: {
+			type: Sequelize.STRING(60),
+			allowNull: false
+		}
 	},
-	email: {
-		type: Sequelize.STRING(60),
-		allowNull: false
-	},
-	password: {
-		type: Sequelize.STRING(60),
-		allowNull: false
+	{
+		hooks: {
+			beforeCreate(usuario) {
+				usuario.password = bcrypt.hashSync(usuario.password, bcrypt.genSaltSync(10));
+			}
+		}
 	}
-});
+);
 
 Usuarios.hasMany(Proyectos);
 
